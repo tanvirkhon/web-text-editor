4d57fb0a6208dade9846c1413873088b5b39fa1d
Hoard Coin <119143763+tanvirkhon@users.noreply.github.com>
2023-04-27 22:42:48 -0500

    Added code to install-js file

--- client/src/js/install.js
+++ client/src/js/install.js
@@ -1,11 +1,27 @@
-const butInstall = document.getElementById('buttonInstall');
+const butInstall = document.getElementById("buttonInstall");
 
 // Logic for installing the PWA
 // TODO: Add an event handler to the `beforeinstallprompt` event
-window.addEventListener('beforeinstallprompt', (event) => {});
+window.addEventListener("beforeinstallprompt", (event) => {
+  // Prevent the mini-infobar from appearing on mobile
+  event.preventDefault();
+  // Stash the event so it can be triggered later.
+  window.deferredPrompt = event;
+});
 
 // TODO: Implement a click event handler on the `butInstall` element
-butInstall.addEventListener('click', async () => {});
+butInstall.addEventListener("click", async () => {
+  // hide our user interface that shows our A2HS button
+  butInstall.classList.add("hidden");
+  // Show the prompt
+  window.deferredPrompt.prompt();
+  // Wait for the user to respond to the prompt
+  const { outcome } = await window.deferredPrompt.userChoice;
+});
 
 // TODO: Add an handler for the `appinstalled` event
-window.addEventListener('appinstalled', (event) => {});
+window.addEventListener("appinstalled", (event) => {
+  event.preventDefault();
+  // Remove the prompt
+  window.deferredPrompt = null;
+});
